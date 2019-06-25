c5990fa7e92dad02c5cef44bae5a64458c8cf0c4
Ryan Liang <ryan7x7x@gmail.com>
2019-06-24 21:31:29 -0700

    fixed bootstrap rendering issue and now match closely to requirements

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,36 +5,43 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <h2>Mortgage Calculator</h2>
-        <form class="form-horizontal" action="/action_page.php">
+        <form className="form-horizontal" action="/action_page.php">
 
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="loanBalance">Loan Balance</label>
-            <div class="col-sm-10">
-              <input type="number" class="form-control" id="loanBalance" placeholder="0" name="balance"/>
+          <div className="form-group">
+              <label className="control-label col-sm-2" ></label>
+            <div className="col-sm-10">
+              <h2>Mortgage Calculator</h2>
+              <hr/>
             </div>
           </div>
 
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="rate">Interest Rate (%)</label>
-            <div class="col-sm-10">          
-              <input type="number" class="form-control" id="rate" placeholder="0" name="rate"/>
+          <div className="form-group">
+            <label className="control-label col-sm-2" for="loanBalance">Loan Balance</label>
+            <div className="col-sm-10">
+              <input type="number" className="form-control" id="loanBalance" placeholder="0" name="balance"/>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label className="control-label col-sm-2" for="rate">Interest Rate (%)</label>
+            <div className="col-sm-10">          
+              <input type="number" className="form-control" id="rate" placeholder="0" name="rate"/>
             </div>
           </div>
           
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="loanTerm">Loan Term (years)</label>
-            <div class="col-sm-10">          
-              <select class="form-control" id="loanTerm" placeholder="15" name="term">
+          <div className="form-group">
+            <label className="control-label col-sm-2" for="loanTerm">Loan Term (years)</label>
+            <div className="col-sm-10">          
+              <select className="form-control" id="loanTerm" placeholder="15" name="term">
                 <option value="15">15</option>
                 <option value="30">30</option>
               </select>
             </div>
           </div>
 
-          <div class="form-group">        
-            <div class="col-sm-offset-2 col-sm-10">
-              <button type="submit" class="btn btn-default" name="submit">Calculate</button>
+          <div className="form-group">        
+            <div className="col-sm-offset-2 col-sm-10">
+              <button type="submit" className="btn btn-default" name="submit">Calculate</button>
             </div>
           </div>
 
